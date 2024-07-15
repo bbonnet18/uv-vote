96729c881839698bb66a3982b21401d611e52ad1
benb <benjamin.a.bonnet@gmail.com>
2024-07-14 23:00:23 -0400

    cosmetic changes

--- uv-vote/src/VoteList.js
+++ uv-vote/src/VoteList.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import Table from "react-bootstrap/Table";
-import  Container  from "react-bootstrap/Container";
+import  { Container, Spinner}   from "react-bootstrap";
 
 function VoteList (props){
   
@@ -16,21 +16,24 @@ function VoteList (props){
         <Table striped bordered hover>
           <thead>
             <tr>
-              <th>Title</th>
-              <th>Link</th>
+              <th>Vote Title</th>
+              <th className='table-link-col'>Link</th>
             </tr>
           </thead>
           <tbody>
             { props.votes ? Object.keys(props.votes).map((itm,ind)=>{
               return <tr key={itm} className={props.votes[itm].link === 'completed' ? 'vote-completed' : ''}> 
               <td>{props.votes[itm].title}</td>
-              <td>{ loadingIds && loadingIds[props.votes[itm].surveyId] ? ('...'):(props.votes[itm].link && props.votes[itm].link !== 'completed' ? (<a href={props.votes[itm].link} alt='Click to vote'>Vote</a>):(props.votes[itm].link === 'completed') ? <div ><img src='./check-square.svg' alt='vote completed'></img></div> : <span><button type='button' onClick={(e)=>{
+              <td className='table-link-col'>{ loadingIds && loadingIds[props.votes[itm].surveyId] ? (
+                <Spinner animation="border" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </Spinner>):(props.votes[itm].link && props.votes[itm].link !== 'completed' ? (<a href={props.votes[itm].link} alt='click or tap to vote' title='click or tap to vote'>Vote</a>):(props.votes[itm].link === 'completed') ? <div ><img src='./check-square.svg' alt='vote completed' title='vote completed'></img></div> : <span><button type='button' onClick={(e)=>{
                 e.preventDefault();
                 let myLid = {...loadingIds};
                 myLid[props.votes[itm].surveyId] = 'loading';
                 setLoadingIds(myLid)
                 props.register(props.votes[itm].surveyId)}
-              } >register</button></span>)}</td>
+              } alt='click or tap to register' title='click or tap to register' >register</button></span>)}</td>
             </tr>
             }) : <></>}
           
