c4c852d0f6e6326a5c1257cd9921dab6378b292e
benb <benjamin.a.bonnet@gmail.com>
2024-08-10 17:31:01 -0400

    recent

--- uv-vote/src/Info.js
+++ uv-vote/src/Info.js
@@ -16,11 +16,18 @@ return(
  
   <h2>Welcome to U-Vote</h2>
     <p>U-Vote is a voting platform that exists to provide you, the voter, with a direct and convenient way to provide your input to your local, state and federal government. U-Vote votes are non-binding, but address the number one reason we vote - to tell them how we feel!</p>
-    <p><a href="/getkey" alt="get a voter key">Get a voter key</a></p>
+    <p><a href="/getkey" alt="get a voter key">Get a voter key</a>   
+    <br></br>
+    <br></br>
+    <h4>See how registration works</h4></p>
+    <div><iframe width="560" height="315" src="https://www.youtube.com/embed/v5ZF-8x6C7k?si=gyLEvsXiOfi94ikg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>
 <hr></hr>
 <h2>What is U-Vote?</h2>
 <p>U-Vote is a modern voting system. Our venerable voting system has served us for over 200 years and remains the binding way we make decisions on our leaders and our intentions at the federal, state and local level. But the system has its limitations. For example, we only vote once a year and only on a few issues and candidates. At U-Vote, we think we can do better. We all know that there are far more issues of concern than those that end up on the ballot each year. U-Vote intends to close that gap between the relatively small amount covered on a typical ballot and how voters really feel about issues both on and off the ballot. 
 </p>
+<Row>
+
+</Row>
 <Row>
     <Col lg={6} sm={12} className='mt-4'>
     <Card >
@@ -28,7 +35,7 @@ return(
       <Card.Body>
         <Card.Title>Modern Voting</Card.Title>
         <Card.Text>
-          Voting that's convenient and feels like it should for the 21st century. 
+          Voting that's convenient and feels like it should in the 21st century. 
         </Card.Text>
         {/* <Card.Link href="/about">See how we do it</Card.Link> */}
       </Card.Body>
