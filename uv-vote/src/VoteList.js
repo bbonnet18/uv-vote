1f2e780211cb7748316a8e535023519f31fd8008
benb <benjamin.a.bonnet@gmail.com>
2024-09-27 23:09:15 -0400

    created separate tables for each group

--- uv-vote/src/VoteList.js
+++ uv-vote/src/VoteList.js
@@ -5,15 +5,61 @@ import  { Container, Spinner}   from "react-bootstrap";
 function VoteList (props){
   
     const [loadingIds,setLoadingIds] = useState({});
+    const [groups,setGroups] = useState([]); // holds the voting groups
 
     useEffect(()=>{
-      console.log('votes changed')
+      console.log('votes changed');
       setLoadingIds({});
+      let voteGroups = {};
+      if(props.votes && props.votes.length){
+        // create an array within the vote group if one doesn't exist
+        props.votes.map((vote)=>{
+          if(voteGroups[vote.group]){
+            voteGroups[vote.group].push(vote);
+          }else{
+            voteGroups[vote.group]=[];
+            voteGroups[vote.group].push(vote);
+          }
+        });
+      }
+      setGroups(voteGroups)
     },[props.votes])
 
     return (
         <Container fluid="md">
-        <Table striped bordered hover>
+       
+          {groups ? Object.keys(groups).map((itm,ind)=>{
+           return (
+            <Table striped bordered hover>   
+              <>
+              <thead>
+                <tr>
+                  <th>{itm}</th>
+                  <th className='table-link-col'>Link</th>
+                </tr></thead>
+              <tbody>
+              {
+                groups[itm] ? groups[itm].map((itm,ind)=>{
+                  return (<tr key={itm} className={itm.link === 'completed' ? 'vote-completed' : ''}> 
+                  <td>{itm.title}</td>
+                  <td className='table-link-col'>{ loadingIds && loadingIds[itm.surveyId] ? (
+                    <Spinner animation="border" role="status">
+                      <span className="visually-hidden">Loading...</span>
+                    </Spinner>):(itm.link && itm.link !== 'completed' ? (<a href={itm.link} alt='click or tap to vote' title='click or tap to vote'>Vote</a>):(itm.link === 'completed') ? <div ><img src='./check-square.svg' alt='vote completed' title='vote completed'></img></div> : <span><button type='button' onClick={(e)=>{
+                    e.preventDefault();
+                    let myLid = {...loadingIds};
+                    myLid[itm.surveyId] = 'loading';
+                    setLoadingIds(myLid)
+                    props.register(itm.surveyId)}
+                  } alt='click or tap to register' title='click or tap to register' >register</button></span>)}</td>
+                </tr>)
+              }) : (<></>)}
+              </tbody>
+              </>
+              </Table>
+            )
+          }):(<></>)}
+           <Table striped bordered hover>
           <thead>
             <tr>
               <th>Vote Title</th>
