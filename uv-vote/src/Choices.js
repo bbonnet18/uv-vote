40e2bf269c7ae73168ea224ec8fe2709dc65d7fb
Benjamin Bonnet <benjaminbonnet@Benjamins-MacBook-Pro.local>
2025-06-01 16:01:12 -0400

    added a couple cards and links

--- uv-vote/src/Choices.js
+++ uv-vote/src/Choices.js
@@ -7,16 +7,39 @@ function Choices() {
     return (
         <Container>
             <Row>
-                <Col lg="12">
-                    <Card style={{ width: '18rem' }}>
+                <Col lg={3} xs={6}>
+                    <Card >
                         <Card.Img variant="top" src="holder.js/100px180" />
                         <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
+                            <Card.Title>Conduit</Card.Title>
                             <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
+                                Conduit let's you say what on your mind about a variety of issues. 
                             </Card.Text>
                             <NavLink to="/actions/conduit">Check out Conduit</NavLink>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col lg={3} xs={6}>
+                    <Card >
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Connect</Card.Title>
+                            <Card.Text>
+                                Connect with candidates
+                            </Card.Text>
+                            <NavLink to="/actions/conduit">Start connecting</NavLink>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col lg={3} xs={6}>
+                    <Card >
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>U-Vote Solutions Teams</Card.Title>
+                            <Card.Text>
+                               Engage with other voters to solve problems in your community.
+                            </Card.Text>
+                            <NavLink to="/actions/conduit">Learn more</NavLink>
                         </Card.Body>
                     </Card>
                 </Col>
