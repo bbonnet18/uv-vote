326176759c2f2238c7e22d9d5655b31284791059
Ben Bonnet <benjamin.a.bonnet@gmail.com>
2025-10-08 17:02:37 -0400

    started breaking down the csv data

--- uv-vote/src/Chart.js
+++ uv-vote/src/Chart.js
@@ -12,6 +12,72 @@ function Chart(){
             console.error("Error loading CSV data:", error);
         }
     }
+
+    // grab just the questions, they will be after the summary
+    const getQuestions =  (data)=> {
+        let questions = [];
+        let isQuestion = false;
+        let isAnswer = false; 
+        let questionsObj = {};
+        let count = 0; 
+        data.forEach(row => {
+            // Extract headings or process each row as needed
+          
+                if(isQuestion){
+                    let questionObj = {
+                        question:row.answer,
+                        labels:Object.keys(row),
+                        data:[]
+                    }
+                    questionsObj[`question${count}`] = questionObj;
+                    isAnswer = true;
+                    isQuestion = false;
+                    count++;
+                }
+                if(checkForSummary(row)){
+                    isQuestion = true;
+                }
+
+                if(checkForData(row)){
+                    questionsObj[`question${count-1}`].data.push(row); 
+                }
+                
+        }
+        )
+
+        return questions; 
+
+    }
+    // looks at a row and determines if it's a data row
+    const checkForData = (row) => {
+        let isData = false;
+        if(row.percentage.indexOf('%') > -1){
+            isData = true;
+        }
+        return isData; 
+    }
+
+    const checkForSummary = (row) => {
+        let isSummary = false;
+        if(row.answer.indexOf('Summary') > -1){
+            isSummary = true;
+        }
+        return isSummary; 
+    }
+    // return data with correct column names
+    const treatData = (data) => {
+        let keyLabels = ["count","answer","percentage"];
+        let treatedData = data.map((itm,ind)=>{
+                let retObj = {};
+                let mKeys = Object.keys(itm);
+                for(let mKey in mKeys){
+                    retObj[keyLabels[mKey]] = itm[mKeys[mKey]];
+                }
+                return retObj;
+        })
+
+        return treatedData;
+    }
     // this will process the CSV data to extract relevant information
     const processData = (data) => {
         try{
@@ -21,37 +87,40 @@ function Chart(){
                 let isData = false;//this is a flag that will be used to know when we're hitting data
                 let isQuestion = false
                 let qCount = 0;
-                data.forEach(row => {
-                    // Extract headings or process each row as needed
-                    if(row['Number of records in this query:']){
-                        if(row['Number of records in this query:'].indexOf('Summary') !== -1){
-                            isData = true;
-                            isQuestion = true;
-                        }
-                    }
-                    if(isData && isQuestion){
-                           headings[`Q${qCount}`] = {question: row['Number of records in this query:']};
-                           isQuestion = false;
-                           qCount++;
-                    }else if(isData && !isQuestion){
-                        let keys = Object.keys(row);
-                        // Process the data rows here using keys
-                        // check for labels
-                        if(row[keys[0]]==='Count' && row[keys[1]] === 'Answer' && row[keys[2]] === 'Percentage') {
-                            // Additional processing can be done here
-                            headings[`Q${qCount - 1}`].labels = ['Count','Answer','Percentage'];
-                            headings[`Q${qCount - 1}`].data = [];
-                        }else{
-                            headings[`Q${qCount - 1}`].data.push({
-                                count: row[keys[0]],
-                                answer: row[keys[1]],
-                                percentage: row[keys[2]]
-                            });
-                        }
-                    }
+                let treated = treatData(data); 
+                let questions = getQuestions(treated);
+                //console.log('questions: ',data); 
+                // data.forEach(row => {
+                //     // Extract headings or process each row as needed
+                //     if(row['Number of records in this query:']){
+                //         if(row['Number of records in this query:'].indexOf('Summary') !== -1){
+                //             isData = true;
+                //             isQuestion = true;
+                //         }
+                //     }
+                //     if(isData && isQuestion){
+                //            headings[`Q${qCount}`] = {question: row['Number of records in this query:']};
+                //            isQuestion = false;
+                //            qCount++;
+                //     }else if(isData && !isQuestion){
+                //         let keys = Object.keys(row);
+                //         // Process the data rows here using keys
+                //         // check for labels
+                //         if(row[keys[0]]==='Count' && row[keys[1]] === 'Answer' && row[keys[2]] === 'Percentage') {
+                //             // Additional processing can be done here
+                //             headings[`Q${qCount - 1}`].labels = ['Count','Answer','Percentage'];
+                //             headings[`Q${qCount - 1}`].data = [];
+                //         }else{
+                //             headings[`Q${qCount - 1}`].data.push({
+                //                 count: row[keys[0]],
+                //                 answer: row[keys[1]],
+                //                 percentage: row[keys[2]]
+                //             });
+                //         }
+                //     }
 
 
-                });
+                //});
             }
         } catch (error) {
             console.error("Error processing data:", error);
