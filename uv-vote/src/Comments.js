c783c1f7da250c59bfb9c5c9f35d3d95325ab67b
benb <benjamin.a.bonnet@gmail.com>
2025-10-27 06:39:25 -0400

    fixed the escaped characters issue

--- uv-vote/src/Comments.js
+++ uv-vote/src/Comments.js
@@ -2,13 +2,18 @@ import './App.css';
 import unescape from 'validator/lib/unescape';
 import { Row } from "react-bootstrap";
 function Comments(props) {
-
+    // for some reason we need to double unescape to get rid of all the escape characters
+    const cleanComment = (comment) => {
+        let cm = unescape(comment).trim();
+        let final = unescape(cm);
+        return final;
+    }
     return (
         <Row className="comments-section">
             {props && props.comments ? (props.comments.map((comment, ind) => {
                 return (
                 <div key={`comment-${ind}`} className="comment-item">
-                    <div key={`comment-text-${ind}`}>{`"${unescape(comment.comment).trim()}"`}</div>
+                    <div key={`comment-text-${ind}`}>{`"${cleanComment(comment.comment)}"`}</div>
                     <div className="comment-meta"><span>Gender: {comment.gender}</span> | <span>Age: {comment.age}</span> | <span>{comment.timestamp}</span></div>
                 </div>);
             })) : (<div>No comments yet</div>)}
