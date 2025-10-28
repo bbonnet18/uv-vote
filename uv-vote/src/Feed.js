12000d6dc5431feb49f02f729f7810a658f39385
Ben Bonnet <benjamin.a.bonnet@gmail.com>
2025-10-28 10:23:18 -0400

    local

--- uv-vote/src/Feed.js
+++ uv-vote/src/Feed.js
@@ -10,56 +10,7 @@ import { useNavigate } from 'react-router-dom';
 import cookies from './cookies';
 
 function Feed (props){ 
-    const [loading, setLoading] = useState(false);
-    const [feed, setFeed] = useState({});
-    const [feedChecked,setFeedChecked] = useState(false);
 
-    // useEffect(() => {
-    //     const checkFeed = async () => {
-    //         await getFeed(props.groupId, props.topicId);
-    //     };
-    //     console.log('rerendering ---- ');
-    //     if(feedChecked===false){
-    //         checkFeed();
-    //         setFeedChecked(true);
-    //     }
-      
-    // }, [props.groupId, props.topicId]);
-
-
-
-    // const getFeed = async(groupId,topicId) => {
-    //     try{
-
-    //          // get the JWT to use for auth
-    //         const authCookie = cookies.getCookie('voterToken') || "";
-    //         if (authCookie === "") {
-    
-    //         navigate('/validate');
-    //         }
-    //         // get the cookie and set the auth header
-    //         const reqOpts = {
-    //         headers: {
-    //             "Authorization": `Bearer ${authCookie}`
-    //         },
-    //         withCredentials: true
-    //         }
-    //         setLoading(true);
-    //         let payload = {
-    //             groupId:groupId,
-    //             topicId:topicId
-    //         }
-    //         const resObj = await axios.post(`${config.apiBaseUrl}/votes/get-feed`, payload, reqOpts);
-    //         setFeed(resObj.data);
-    //         setLoading(false);
-    //         } catch (error) {
-    //             console.error("Error fetching feed:", error);
-    //             setLoading(false);
-    //     }
-
-    
-        
-    // }
     return (
         <Container>
             {loading ? (<div className="comment-loading loading-centered"><Spinner animation="border" role="status" className='loading-spinner'> <span className="visually-hidden">Loading...</span></Spinner></div>
