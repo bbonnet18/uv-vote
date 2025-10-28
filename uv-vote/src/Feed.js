eef880440d6da274118585446103790c87c580c2
Ben Bonnet <benjamin.a.bonnet@gmail.com>
2025-10-28 10:32:15 -0400

    fixed local Feed file

--- uv-vote/src/Feed.js
+++ uv-vote/src/Feed.js
@@ -1,3 +1,4 @@
+
 import './App.css';
 import { useState, useEffect } from 'react';
 import { Container, Row, Col, Spinner} from "react-bootstrap";
@@ -5,7 +6,56 @@ import Comments from './Comments';
 import VoteChart from './VoteChart';
 
 function Feed (props){ 
+    const [loading, setLoading] = useState(false);
+    const [feed, setFeed] = useState({});
+    const [feedChecked,setFeedChecked] = useState(false);
 
+    // useEffect(() => {
+    //     const checkFeed = async () => {
+    //         await getFeed(props.groupId, props.topicId);
+    //     };
+    //     console.log('rerendering ---- ');
+    //     if(feedChecked===false){
+    //         checkFeed();
+    //         setFeedChecked(true);
+    //     }
+      
+    // }, [props.groupId, props.topicId]);
+
+
+
+    // const getFeed = async(groupId,topicId) => {
+    //     try{
+
+    //          // get the JWT to use for auth
+    //         const authCookie = cookies.getCookie('voterToken') || "";
+    //         if (authCookie === "") {
+    
+    //         navigate('/validate');
+    //         }
+    //         // get the cookie and set the auth header
+    //         const reqOpts = {
+    //         headers: {
+    //             "Authorization": `Bearer ${authCookie}`
+    //         },
+    //         withCredentials: true
+    //         }
+    //         setLoading(true);
+    //         let payload = {
+    //             groupId:groupId,
+    //             topicId:topicId
+    //         }
+    //         const resObj = await axios.post(`${config.apiBaseUrl}/votes/get-feed`, payload, reqOpts);
+    //         setFeed(resObj.data);
+    //         setLoading(false);
+    //         } catch (error) {
+    //             console.error("Error fetching feed:", error);
+    //             setLoading(false);
+    //     }
+
+    
+        
+    // }
     return (
         <Container className="feed-container">
             <Row><hr/></Row>
